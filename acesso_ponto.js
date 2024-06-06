a18c1479b3a77ca2ddb193381b5d5b50edd92580
isaacraf <141454341+isaacraf@users.noreply.github.com>
2024-06-06 11:27:47 -0300

    Add files via upload

---
+++ acesso_ponto.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "isaac",
+    idade: 17,
+    cpf: "1122233345",
+    email: "isaac@dominio.com",
+};
+
+console.log(cliente)
---
+++ objeto.js
@@ -0,0 +1,11 @@
+const listaCPFS = [1111111, 22222, 3333];
+
+const informacoesPessoa = ["nome", "isaac", "idade", 17, "CPF", "1111222333"]; 
+
+console.log(informacoesPessoa(1))
+
+const objetoPessoa  = {
+  nome: "isaac",
+  idade: 17,
+  
+}
